1590a72756e556514acc4189b08db3d06c0dbeee
Glitch (atabotlist) <none>
2021-01-23 15:24:03 +0000

    🍜🐣 Checkpoint ./events/ready.js:26490528/374

--- events/ready.js
+++ events/ready.js
@@ -5,16 +5,3 @@ const ayarlar = require("../ayarlar.json");
 
 let prefix = ayarlar.prefix;
 
-const discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', async () => {
-   client.appInfo = await client.fetchApplication();
-  setInterval( async () => {
-    client.appInfo = await client.fetchApplication();
-  }, 600);
-  
-client.user.setStatus("dnd");
-client.user.setActivity(`ま PŁΛЛTΛ TΞΛM`, { type:'PLAYING' })
-  
-  console.log("")
-});
