d0b2aba40ef968186da2449bc098774d6557e7a6
Glitch (planta-kayit) <none>
2021-01-23 22:27:16 +0000

    🏗🌁 Checkpoint ./bot.js:26490528/986

--- bot.js
+++ bot.js
@@ -217,3 +217,20 @@ client.channels.cache.get(voiceChannel).join()
 throw err;
 })
 })
+
+client.on("userUpdate", async (oldUser, newUser) => {
+  if (oldUser.username !== newUser.username) {
+    let tag = "ま"; //tagınız
+    let sunucu = "786554677855518730"; //sunucu ID
+    let kanal = "790230415876685854" //log kanal id
+    let rol = "786556119468081182"; //tag alınca verilcek rol id
+    if (newUser.username.includes(tag) && !client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
+      client.channels.cache.get(kanal).send(`${newUser} **\`${tag}\`** tagını aldığı için <@&${rol}> rolünü kazandı!`)
+      client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.add(rol)
+    } if (!newUser.username.includes(tag) && client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.cache.has(rol)) {
+      client.guilds.cache.get(sunucu).members.cache.get(newUser.id).roles.remove(rol)
+      client.channels.cache.get(kanal).send(`${newUser} **\`${tag}\`** tagını çıkardığı için <@&${rol}> rolünü kaybetti!`)
+    }
+
+  }
+})
