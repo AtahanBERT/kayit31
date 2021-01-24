403ec9e01c374c148f9c5b964369ca08bd06cab2
Glitch (planta-kayit) <none>
2021-01-24 11:15:58 +0000

    🛰👸 Checkpoint ./komutlar/yapimcim.js:26490528/480

--- komutlar/yapimcim.js
+++ komutlar/yapimcim.js
@@ -5,20 +5,7 @@ exports.run = (client, message) => {
   if (message.channel.type !== 'dm') {
     var embed = new Discord.MessageEmbed()
     .setTitle("Yapımcım Adam Gibi Adam")
-    .setDescription("<@429357746002067493> | ! ま Atahan Ŧαякєтмєz#1353")
-    .setImage('https://dynamic.brandcrowd.com/asset/logo/2fa9614c-e8fd-47be-b522-14aa07bd8f22/logo?v=4&text=Atahan')
-    .setColor("#2c2f33")
-    message.channel.send({embed})
-}};
-
-exports.conf = {
-  enabled: true,
-  guildOnly: false,
-  aliases: ['yapımcım','yapımcı','yapımcılarım','yapımcılar','yapimcim','yapimci',],
-  permLevel: 0
-};
-
-exports.help = {
+    .setDescription("<@429357746002067493>,<@440521528770035714>
   name: 'yapımcım',
   kategori: 'genel',
   description: 'Yapımcımı Gosterir.',
