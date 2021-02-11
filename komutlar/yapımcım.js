e700ffea2b57d8cb4897300a4280681679d05e66
Glitch (planta-kayit) <none>
2021-02-11 21:31:21 +0000

    👝🏖 Checkpoint ./komutlar/yapımcım.js:26490528/559 ./.glitch-assets:26490528/526 ./bot.js:26490528/211

--- komutlar/yapımcım.js
+++ komutlar/yapımcım.js
@@ -6,7 +6,7 @@ exports.run = (client, message) => {
     var embed = new Discord.MessageEmbed()
     .setTitle("Yapımcım Adam Gibi Adam")
     .setDescription("<@429357746002067493> | ! ま Atahan Ŧαякєтмєz#0001")
-    .setImage('https://images-ext-2.discordapp.net/external/dwVm_qFu7T7a4Rsj_MScV8tS3t5J5ekX960xaeoQEiQ/https/images-ext-1.discordapp.net/external/gCG42Oo2aXDL7c-yZJ6YMSfvcwT58Sc00k-OKLlpkG4/https/media0.giphy.com/media/NKEt9elQ5cR68/200.gif')
+    .setImage('https://images-ext-2.discordapp.net/external/sKcMKrk3CMWx0rccRtVo8uoGOIX06juRHqBT3O4QCJo/https/media1.tenor.com/images/c56e262cc5781c99ab1ca33a5d0c6bfe/tenor.gif')
     .setColor("#2c2f33")
     message.channel.send({embed})
 }};
