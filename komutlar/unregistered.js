8cce6decb44a2c763c59162c0a585329d4cdcb49
Glitch (planta-kayit) <none>
2021-03-09 21:31:04 +0000

    🐔🏙 Checkpoint ./komutlar/unregistered.js:26490528/67 ./ayarlar.json:26490528/78

--- komutlar/unregistered.js
+++ komutlar/unregistered.js
@@ -5,7 +5,7 @@ const db = require('quick.db');
 
 exports.run = async (client, message, args) => {
  
-    let yetkili = ayarlar.kyetkili;
+    let yetkili = ayarlar.yetkiliROL;
     let unregister = ayarlar.kayıtsızROL;
     let kullanıcı = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
     let member = message.guild.member(kullanıcı);
@@ -16,7 +16,7 @@ exports.run = async (client, message, args) => {
 
    if (!message.member.roles.cache.get(yetkili) & !message.member.hasPermission("ADMINISTRATOR")) return message.react(basarisiz);
    if (!kullanıcı) return message.channel.send(new Discord.MessageEmbed().setColor("RED").setDescription('<a:plantacarp:815252488168931368> Kayıtsıza atabilmek için bir kullanıcı belirtmelisin!')).then(x => x.delete({timeout: 3000}));
-   if (message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:qmi2:809010861162233857> Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!`)).then(x => x.delete({timeout: 5000}));
+   if (message.member.roles.highest.position <= member.roles.highest.position) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!`)).then(x => x.delete({timeout: 5000}));
    if (!ayarlar.sahip) return message.channel.send(new Discord.MessageEmbed().setColor('RED').setDescription(`<a:plantacarp:815252488168931368> Sahibimin üzerinde komut kullanamazsın!`));
 
 member.roles.cache.forEach(r => {
