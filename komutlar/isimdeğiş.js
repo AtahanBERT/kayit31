46e12ef5fe95dbb21c702f782b14c999b78ed638
Glitch (planta-kayit) <none>
2021-03-16 22:55:09 +0000

    🎖🍩 Checkpoint ./komutlar/isimdeğiş.js:26490528/59

--- komutlar/isimdeğiş.js
+++ komutlar/isimdeğiş.js
@@ -7,6 +7,7 @@ exports.run = async (client, message, args) => {
 
 if (!message.member.permissions.has("MANAGE_GUILD")) return message.reply(`Bu Komutu Kullanmak İçin **Yetkili** Olmalısın!`); 
 
+let tag = ayarlar.tag;
 let isim = args.slice(1).join(' ');
 let kullanici = message.mentions.users.first();
   
@@ -16,13 +17,13 @@ if(!isim) return message.reply(`Lütfen bir kullanıcı adı giriniz! \nDoğru K
 if(isim.length > 32) return message.reply(`Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`)
   
 message.guild.members.cache.get(kullanici.id).setNickname(`${tag} ${isim}`)
-message.channel.send(`Başarılı bir şekilde \`${kullanici.username}\` adlı kişinin kullanıcı adı \`${isim}\` olarak değiştirildi.`)
+message.channel.send(`Başarılı bir şekilde \`${kullanici.username}\` adlı kişinin kullanıcı adı \`${tag} ${isim}\` olarak değiştirildi.`)
 }
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
-    aliases: ['isimdegistir','ideğiş'],
+    aliases: ['isimdegistir','ideğiş','idegis'],
     permLevel: 0
 }
 
