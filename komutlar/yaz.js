332d0bcca4b5ae51cfaa573342de91e1585eca84
Glitch (planta-kayit) <none>
2021-03-20 13:04:02 +0000

    🎀💵 Checkpoint ./komutlar/yaz.js:26490528/18

--- komutlar/yaz.js
+++ komutlar/yaz.js
@@ -4,13 +4,13 @@ exports.run = (client, message, args) => {
   let mesaj = args.slice(0).join(' ');
 if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
   message.delete();
-  message.channel.send(mesaj);//sharpen
+  message.channel.send(mesaj);
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ['say', 'söyle'],
+  aliases: ['söyle'],
   permLevel: 2
 };
 
