eca9188bb4c1e6c47c8f2dd899331d4550a513ca
Glitch (planta-kayit) <none>
2021-03-23 09:46:27 +0000

    👂💂 Checkpoint ./komutlar/isim.js:26490528/1214

--- komutlar/isim.js
+++ komutlar/isim.js
@@ -11,15 +11,16 @@ if (!message.member.roles.cache.get(ayarlar.yetkiliROL)) return message.reply(`$
 
 let tag = ayarlar.tag;
 let isim = args.slice(1).join(' ');
-let kullanici = message.mentions.users.first();
+let kullanici = message.guild.member(message.mentions.members.first() || message.guild.members.cache.get(args[0]));
   
 if(!kullanici) return message.reply(`${basarisiz} Lütfen bir kullanıcı giriniz! \nDoğru Kullanım; \`${prefix}isim @üye <yeni isim>\``)
-  
+
+if(message.member.roles.highest.position <= kullanici.roles.highest.position) return message.channel.send(new Discord.MessageEmbed().setDescription(`${basarisiz} ${message.author}, Etiketlenen kullanıcı sizden üst/aynı pozisyondadır.`).setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setColor('0x800d0d').setTimestamp()).then(x => x.delete({timeout: 5000}));
 if(!isim) return message.reply(`${basarisiz} Lütfen bir kullanıcı adı giriniz! \nDoğru Kullanım; \`${prefix}isim @üye <yeni isim>\``)
 if(isim.length > 32) return message.reply(`${basari} Lütfen \`32\` karakteri geçmeyecek şekilde bir isim giriniz!`)
   
 message.guild.members.cache.get(kullanici.id).setNickname(`${tag} ${isim}`)
-message.channel.send(`${basari} Başarılı bir şekilde \`${kullanici.username}\` adlı kişinin kullanıcı adı \`${tag} ${isim}\` olarak değiştirildi.`)
+message.channel.send(new Discord.MessageEmbed().setDescription(`${basari} Başarılı bir şekilde \`${kullanici.username}\` adlı kişinin kullanıcı adı \`${tag} ${isim}\` olarak değiştirildi.`).setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor('0x348f36').setTimestamp())
 }
 
 exports.conf = {
