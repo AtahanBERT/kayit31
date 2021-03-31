13d0ea6a0e305dbdba66693ddb923be21c49321f
Glitch (planta-kayit) <none>
2021-03-31 17:52:10 +0000

    🗺🌂 Checkpoint ./ayarlar.json:26490528/37 ./bot.js:26490528/38

--- bot.js
+++ bot.js
@@ -113,7 +113,7 @@ client.elevation = message => {
 client.on("guildMemberAdd", member => {
   let tag = ayarlar.tag;
   //splashen
-  member.setNickname(ayarlar.kayitsizad);
+  member.setNickname(`${tag}| -İsim Yaş-`);
 });
 
 // İSİM YAŞ İSİM DEĞİŞTİRME SON
