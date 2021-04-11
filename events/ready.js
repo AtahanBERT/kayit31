ff5e629a9477def184b62483c6692d23735de6df
Glitch (planta-kayit) <none>
2021-04-11 18:28:25 +0000

    Rewound to commit 07f438493875a5544717135d7a98707ef5d03a63 + 0 edits

--- events/ready.js
+++ events/ready.js
@@ -3,10 +3,12 @@ const moment = require("moment");
 const Discord = require("discord.js");
 const ayarlar = require("../ayarlar.json");
 
+let prefix = ayarlar.prefix;
 let durum = ayarlar.durum;
 
 module.exports = client => {
-  let durum = ayarlar.durum
-client.user.setActivity(durum, {type: 'PLAYING'}); 
+  client.user.setStatus("online");
 
-}
+client.user.setActivity(durum)
+};
+
