347b85f1bbc386e85bda95f195ddb58deee245ab
Glitch (planta-kayit) <none>
2021-04-11 17:15:09 +0000

    💿🌉 Checkpoint ./events/message.js:26490528/708

--- events/message.js
+++ events/message.js
@@ -1,6 +1,7 @@
 const ayarlar = require('../ayarlar.json');
 module.exports = message => {
   const Discord = require('discord.js')
+  let basarisiz = ayarlar.basarisizemoji;
   let client = message.client;
   if (message.author.bot) return;
   if (!message.content.startsWith(ayarlar.prefix)) return;
@@ -9,11 +10,15 @@ module.exports = message => {
   let perms = client.elevation(message);
   let cmd;
   
-  if (client.commands.has(command)) {
-    cmd = client.commands.get(command);
-  } else if (client.aliases.has(command)) {
-    cmd = client.commands.get(client.aliases.get(command));
-  }
+  if (!client.commands.has(command)) {
+    if (client.aliases.has(command)) {
+      cmd = client.commands.get(client.aliases.get(command));
+    } else {
+      if(command == '') return;
+message.channel.send(new Discord.MessageEmbed()
+.setDescription(`${basarisiz} Botta **` + command + '** Adında Bir Komut Bulunamadı.')
+.setColor('0x800d0d').setAuthor(message.member.displayName, message.author.avatarURL({ dynamic: true })).setTimestamp()).then(x => x.delete({timeout: 5000}));
+  }}
   if (cmd) {
     if (perms < cmd.conf.permLevel) return;
     cmd.run(client, message, params, perms);
