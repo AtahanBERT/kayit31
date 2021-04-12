c22a4b1975d12f3c355e3cc9cb5e4f28a6938a98
Glitch (planta-kayit) <none>
2021-04-12 15:14:57 +0000

    💓📅 Checkpoint ./events/ready.js:26490528/745

--- events/ready.js
+++ events/ready.js
@@ -3,12 +3,9 @@ const moment = require("moment");
 const Discord = require("discord.js");
 const ayarlar = require("../ayarlar.json");
 
-let prefix = ayarlar.prefix;
-let durum = ayarlar.durum;
-
 module.exports = client => {
   client.user.setStatus("online");
 
-client.user.setActivity(durum)
+  let durum = ayarlar.durum;
+client.user.setActivity(durum, {type: 'PLAYING'}); 
 };
-
