31a018fc9ba0bc7f7787461588632b74fe3b9ada
Glitch (planta-kayit) <none>
2021-04-17 13:25:25 +0000

    🚿💺 Checkpoint ./komutlar/kayıtpuan.js:26490528/404

--- komutlar/kayıtpuan.js
+++ komutlar/kayıtpuan.js
@@ -1,5 +1,5 @@
 const { Discord, MessageEmbed } = require('discord.js');
-const db = require("quick.db")
+const db = require('quick.db')
 let ayarlar = require('../ayarlar.json')
 let emoji = ayarlar.basarisizemoji;
 exports.run = async (client, message, args) => {
@@ -11,10 +11,10 @@ if(!message.member.roles.cache.has(ayarlar.yetkiliROL) & !message.member.hasPerm
   let rol = message.mentions.roles.first()
   let member = message.guild.member(üye)
   let erkekpuanlar = db.fetch(`kayıt.puan.erkek.${üye.id}`)
-    let kadınpuanlar = db.fetch(`kayıt.puan.kadın.${üye.id}`)
-    let total = kadınpuanlar + erkekpuanlar
-  let lecrain = new Discord.MessageEmbed()
-  .setAuthor(`Kayıt Puan | Kişi : ${üye.username}`)
+  let kadınpuanlar = db.fetch(`kayıt.puan.kadın.${üye.id}`)
+  let total = kadınpuanlar + erkekpuanlar
+  let lecrain = new MessageEmbed()
+  .setAuthor(`Kayıt Puan | Kişi : ${üye}`)
   .setColor('BLACK')
   .setDescription(`
 <a:mega:828241745862066206> Toplam Kadın Kayıtı Puanın : **${kadınpuanlar ? kadınpuanlar : 'Hiç Kadın Birini Kayıt Etmedin'}** 
@@ -30,14 +30,12 @@ if(!message.member.roles.cache.has(ayarlar.yetkiliROL) & !message.member.hasPerm
 };
 
 exports.conf = {
-  enabled: false,
+  enabled: true,
   guildOnly: false,
-  permlevel: 0,
-  aliases: ["stats","stat","kaçkişiyikayıtettim","kayıtpuan"],
+  aliases: ['kayıt','stat'],
+  permLevel: 0
 };
-  
+
 exports.help = {
-  name: 'kayıtpuan',
-  description: 'kayıt',
-  usage: 'kayıtpuan @üye'
+  name: 'kadın'
 }
